ca5e625a19a80a98b3a6d7e98d55f5a586c8ed94
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro-2.local>
2023-01-15 12:44:03 +0530

    add Recipedb class in models.py in adminapp

--- Recipe_adminapp/models.py
+++ Recipe_adminapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipedb(models.Model):
+    recipe_name = models.CharField(max_length=30),
+    recipe_image = models.ImageField(upload_to='SampleImage', default='null.jpg'),
+    instruction = models.TextField(),
+    ingredients = models.TextField
+
+def __str__(self):
+    return self.name
