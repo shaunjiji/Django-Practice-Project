eee5c25633dfe8328e7fa43df47f8017113376f7
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro-2.local>
2023-01-15 12:21:28 +0530

    include paths for both user and admin apps

--- RecipeProject/urls.py
+++ RecipeProject/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('recipe_admin/', include('Recipe_adminapp.urls')),
+    path('recipe_userapp', include('Recipe_userapp'))
 ]
