c541a3e6a61d24b517027cb5f2822178e7a066c8
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro-2.local>
2023-01-16 11:29:30 +0530

    add views method for register(post route)

--- Recipe_userapp/urls.py
+++ Recipe_userapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('register/', views.register)
+path('register/', views.register, name='register'),
+path('register_account', views.register_account, name='register_account')
 ]
 
--- Recipe_userapp/views.py
+++ Recipe_userapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.shortcuts import render
+from . models import *
 
 # Create your views here.
 
 def register(request):
-    return HttpResponse("Hello Worlds")
+    return render (request, 'register.html')
+
+def send_message(request):
+    if request.method == 'POST':
+        user_name = request.POST['recipe']
+        user_email = request.POST['email']
+        user_subject = request.POST['subject']
+        user_message = request.POST['message']
+    data = Contactdb(name = user_name, email = user_email, subject = user_subject, message = user_message)
+    data.save()
+    return redirect('register')
