de6da2dd4753c5465dc67cdab4104909ec26f568
Shaun Jiji <shaunjiji@Shauns-MacBook-Pro-2.local>
2023-01-16 11:14:58 +0530

    create Contactdb class and register model

--- Recipe_userapp/models.py
+++ Recipe_userapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contactdb(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    message = models.TextField()
+    subject = models.TextField()
+
+    def __str__(self):
+        return self.name
--- Recipe_userapp/views.py
+++ Recipe_userapp/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(request):
+    return HttpResponse("Hello Worlds")
